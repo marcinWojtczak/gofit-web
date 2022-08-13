562f461e9dcd38eedcf132ebcdc81a1fe71f6f8a
marcinWojtczak <marcinwojtczak.pure@gmail.com>
2022-08-13 15:33:32 +0200

    changed config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
-from dotenv import load_dotenv
+
 
 class Config:
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-    FLASKS3_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = 'eu-central-1'
     ACL = 'public-mode'
